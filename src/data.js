fec5d4b97cd2e9bb386fecce42d5ba2850a90d91
Parth Kapil <parthkapil@parths-mbp.attlocal.net>
2022-09-26 12:55:39 -0400

    branch check

--- src/data.js
+++ src/data.js
@@ -2,38 +2,38 @@
 // parameters for Navigation bar
 /////////////////////////////////////////////////////
 export const navBarData = {
-	logo: "LOGO",
+	logo: "ICE",
 	btnText: "Nav Button", // text for the button
 	btnURL: "https://groupme.com/join_group/76453291/e6j4ng9s", // redirect URL on button click
-	link1: "link1", // name of link
-	link2: "link2",
-	link3: "link3",
+	link1: "June", // name of link
+	link2: "July",
+	link3: "August",
 };
 
 export const heroData = {
-	heading: "This is the heading",
-	moto: "This is my moto",
-	btnText: "button text",
+	heading: "Hey! this is a dummy website",
+	moto: "This is easy",
+	btnText: "Isn't it",
 };
 
 export const pageObjOne = {
-	id: "link1", // id of the page. it should be the same as name of the link in navbar. (eg-> if link1=about then here id should be about)
+	id: "June", // id of the page. it should be the same as name of the link in navbar. (eg-> if link1=about then here id should be about)
 	lightBg: true, // true for white, false for black
 	lightText: false, // true for white, false for black
 	lightTextDesc: false, // true for white, false for black
-	topLine: "This is top line",
+	topLine: "Thsi is page one",
 	headLine: "This is headline",
 	description: "Description text here!!",
 	darkText: true, // description color: false -> black, true-> white
 
 	//image params
 	imgStart: false, // image on left or right
-	img: "svg-1.svg", // image path. file should be in the images folder
+	img: "ice_car.svg", // image path. file should be in the images folder
 	alt: "Image not there", // this text will show if we are unable to find the file (if you are getting this maybe the file name is wrong or the file in not present in the images folder)
 
 	//these are the button params
 	buttonRequired: true, // need button on page or not
-	buttonLabel: "Become a member!", // button text
+	buttonLabel: "what's uppppppp!", // button text
 	dark: false, // button text black or not
 	primary: false, // button background: false -> black, true-> white
 };
@@ -62,8 +62,8 @@ export const pageObjTwo = {
 
 export const teamData = [
 	{
-		name: "Member 1",
-		designation: "Designation 1",
+		name: "Parth kapil",
+		designation: "SDE 1",
 		imgPath: "profile.svg",
 	},
 	{
